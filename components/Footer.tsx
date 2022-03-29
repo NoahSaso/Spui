038befb4233a8c1cfd0e716bb8dbedafef3c7c8c
Noah Saso <noahsaso@gmail.com>
2022-03-29 12:30:06 -0700

    Rewrote footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames"
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { FunctionComponent } from "react"
 import { IconType } from "react-icons"
 import { CgPlayList } from "react-icons/cg"
 import { IoSearch } from "react-icons/io5"
@@ -8,80 +9,82 @@ import { MdAccountCircle } from "react-icons/md"
 import { useRecoilValue } from "recoil"
 
 import { isLoggedInSelector } from "@/state"
-
-interface Tab {
-  label: string
-  icon: {
-    inactive: IconType
-    active: IconType
-  }
-  href: string
-  startsWith?: string
-}
-
-const authTabs: Tab[] = [
-  {
-    label: "Search",
-    icon: {
-      inactive: IoSearch,
-      active: IoSearch,
-    },
-    href: "/search",
-  },
-  {
-    label: "Playlists",
-    icon: {
-      inactive: CgPlayList,
-      active: CgPlayList,
-    },
-    href: "/playlists",
-    startsWith: "/playlist",
-  },
-]
-
-const unauthTabs: Tab[] = [
-  {
-    label: "Connect",
-    icon: {
-      inactive: MdAccountCircle,
-      active: MdAccountCircle,
-    },
-    href: "/",
-    startsWith: "/",
-  },
-]
+import { colors } from "@/theme"
 
 export const Footer = () => {
   const { pathname } = useRouter()
   const rootPath = "/" + pathname.split("/")[1]
 
   const isLoggedIn = useRecoilValue(isLoggedInSelector)
-  const tabs = isLoggedIn ? authTabs : unauthTabs
 
   return (
     <div className="h-footer bg-card flex flex-row justify-center items-stretch z-10 flex-none sm:rounded-t-md sm:overflow-hidden">
-      {tabs.map(({ label, icon: { inactive, active }, href, startsWith }) => {
-        const isActive = startsWith
-          ? rootPath.startsWith(startsWith)
-          : rootPath === href
-        const Icon = isActive ? active : inactive
-
-        return (
-          <div key={label} className="flex-1 flex justify-center items-center">
-            <Link href={href}>
-              <a
-                className={classNames(
-                  "flex flex-col justify-center items-center opacity-40",
-                  { "opacity-100": isActive }
-                )}
-              >
-                <Icon size={26} />
-                <p className="text-xs">{label}</p>
-              </a>
-            </Link>
-          </div>
-        )
-      })}
+      {(isLoggedIn ? authTabs : unauthTabs).map((tab) => (
+        <Tab
+          key={tab.label}
+          data={tab}
+          isActive={
+            tab.startsWith
+              ? rootPath.startsWith(tab.startsWith)
+              : rootPath === tab.href
+          }
+        />
+      ))}
     </div>
   )
 }
+
+interface TabData {
+  label: string
+  Icon: IconType
+  href: string
+  startsWith?: string
+}
+
+const unauthTabs: TabData[] = [
+  {
+    label: "Connect",
+    Icon: MdAccountCircle,
+    href: "/",
+    startsWith: "/",
+  },
+]
+
+const authTabs: TabData[] = [
+  {
+    label: "Search",
+    Icon: IoSearch,
+    href: "/search",
+  },
+  {
+    label: "Playlists",
+    Icon: CgPlayList,
+    href: "/playlists",
+    startsWith: "/playlist",
+  },
+]
+
+interface TabProps {
+  data: TabData
+  isActive: boolean
+}
+
+const Tab: FunctionComponent<TabProps> = ({
+  data: { label, Icon, href },
+  isActive,
+}) => (
+  <Link href={href}>
+    <a
+      className={classNames(
+        "flex-1 flex flex-col justify-center items-center",
+        {
+          "opacity-40": !isActive,
+          "opacity-100": isActive,
+        }
+      )}
+    >
+      <Icon color={colors.light} size={26} />
+      <p className="text-xs">{label}</p>
+    </a>
+  </Link>
+)
