acacb2ffe785cb49f2ff62ca2b405f8b06c26e42
Noah Saso <noahsaso@gmail.com>
2022-03-31 13:23:23 -0700

    Fixed PWA config.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 const withPWA = require("next-pwa")
 
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-  },
-})(
+const nextConfig = withPWA(
   /** @type {import('next').NextConfig} */
   {
+    pwa: {
+      dest: "public",
+    },
     reactStrictMode: true,
     experimental: {
       emotion: true,
