5217b05c8a04bc80f38f0e8fce12394540499b0c
Noah Saso <noahsaso@gmail.com>
2022-03-31 12:29:43 -0700

    Added pwa.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    emotion: true,
+const withPWA = require("next-pwa")
+
+const nextConfig = withPWA()({
+  pwa: {
+    dest: "public",
   },
-}
+})(
+  /** @type {import('next').NextConfig} */
+  {
+    reactStrictMode: true,
+    experimental: {
+      emotion: true,
+    },
+  }
+)
 
 module.exports = nextConfig
